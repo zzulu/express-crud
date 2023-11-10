5d375f5721f40b5db1f76150ad78d7b6bd98cebe
Junwoo Hwang <zzulu.hwang@gmail.com>
2023-11-10 22:33:34 +0900

    Update: root route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const response = {
-    'status': 'OK',
+  const responseBody = {
+    'status': 200,
+    'message': 'Welcome to the beginning of nothingness.',
   }
-  res.status(200).send(response);
+  res.status(200).send(responseBody);
 });
 
 export default router;
